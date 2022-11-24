fa2e65a65f9d12388b42a481349fe18f15a57dd7
sbstobiask <sbs202223kerchner@gmail.com>
2022-11-24 18:22:12 +0100

    22

--- BlackJack.py
+++ BlackJack.py
@@ -1,3 +1,17 @@
 import random
 
-deck = []
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "B", "D", "K", "A"]*4
+player1 = []
+
+random.shuffle(deck)
+
+print(deck)
+
+player1.append(deck.pop())
+player1.append(deck.pop())
+
+print(player1)
+print(deck)
+
+if player1.count("B") == 1 and player1.count("A") == 1:
+    print("Black Jack!")
